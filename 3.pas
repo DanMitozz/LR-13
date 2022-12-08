4ccbc78feda0d5665208f03c775def7c6e26b318
DanMitozz <117995535+DanMitozz@users.noreply.github.com>
2022-12-08 21:27:49 +0300

    Add files via upload

---
+++ 3.pas
@@ -0,0 +1,11 @@
+﻿var
+   filetext: text;
+   a:string;
+begin
+write('Введите строку: ');
+readln(a);
+assign(filetext,'D:\LR 13\text3.txt');
+append(filetext);
+write(filetext,a);
+close(filetext);
+end.
